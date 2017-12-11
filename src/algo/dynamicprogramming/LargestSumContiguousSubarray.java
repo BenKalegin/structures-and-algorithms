64e8bc2870acd281ef82abb821cedf8007002168
Ben <BenKalegin@users.noreply.github.com>
2017-12-11 18:50:50 -0500

    Largest continous sum

---
+++ src/algo/dynamicprogramming/LargestSumContiguousSubarray.java
@@ -0,0 +1,26 @@
+package algo.dynamicprogramming;
+
+import java.util.Arrays;
+
+public class LargestSumContiguousSubarray {
+    private int solve(int[] numbers) {
+        // recurrence: dp[i] = dp[i-1] + a[i]
+
+        int n = numbers.length;
+        int[] dp = new int[n];
+
+        dp[0] = numbers[0];
+        for(int i = 1; i < numbers.length; i++) {
+            if (dp[i-1] >= 0)
+                dp[i] = dp[i-1] + numbers[i];
+            else
+                dp[i] = numbers[i];
+        }
+        return Arrays.stream(dp).max().getAsInt();
+    }
+
+    public void test() {
+        int[] numbers = new int[] {-2, -3, 4, -1, -2, 1, 5, -3};
+        System.out.println("Lasrgest sum for array " + numbers.toString() + " is " + solve(numbers));
+    }
+}
