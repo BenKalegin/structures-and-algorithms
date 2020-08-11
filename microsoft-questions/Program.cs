d642a9b3d8b60ff63c3fd4b3e097b7934916ce2d
Ben Kalegin <BenKalegin@users.noreply.github.com>
2020-08-11 14:17:25 -0400

    +1

--- microsoft-questions/Program.cs
+++ microsoft-questions/Program.cs
@@ -14,7 +14,7 @@ namespace microsoft_questions
 		// Given an array arr[] of positive integers of size N. Reverse every sub-array of K group elements.
 		static void Main(string[] args)
         {
-            MinimumOperations.Test();
+            MaxLengthChain.Test();
 			//DeleteAGivenNodeFromASortedSinglyLinkedList.Test();
 			//ReverseLinkedList.Test();
 			//SearchInSortedAndRotatedArray.Test();
